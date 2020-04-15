fb454f224de2c08527b27f7ef9e048a208c6c57b
lujiehui <lujiehui@woouo.com>
2020-04-15 09:36:14 +0800

    111

---
+++ lib/components/v-refresh-header.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+
+Header vRefreshHeader = ClassicalHeader(
+    refreshText: '下拉刷新',
+    refreshReadyText: '释放以刷新',
+    refreshingText: '刷新中……',
+    refreshedText: '刷新完成',
+    infoText: '更新于 %T');
